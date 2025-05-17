b50d794b5a5149afb9ef3ab20b6de4a0fefc87af
Aryan Suri <arysuri@proton.me>
2025-05-17 16:43:14 -0700

    rm mc.txt
    Signed-off-by: Aryan Suri <arysuri@proton.me>

---
+++ src/2106C.cpp
@@ -0,0 +1,20 @@
+/**
+ * author: Aryan Suri
+ * date: 2025-05-17T14:39:22-700
+ * licence: MIT
+ **/
+#include <iostream>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+  }
+
+  return 0;
+}
